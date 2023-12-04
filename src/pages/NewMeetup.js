206961b83e88498c8423c7dc6c26ab9fb7d2ad77
Manojkumar <manojprfp@gmail.com>
2023-12-04 20:30:09 +0530

    added meetup form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import {useHistory} from 'react-router-dom';
+import NewMeetupForm from "../meetup/NewMeetupForm";
 const NewMeetup = () => {
+  const history=useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-c81e8-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(()=>{
+      history.replace('/');
+    })
+  }
   return (
-    <div>NewMeetup</div>
-  )
-}
+    <section>
+      <h1>Add NewMeetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+};
 
-export default NewMeetup
+export default NewMeetup;
